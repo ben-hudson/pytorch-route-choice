b7b3f6258f9200ffa74a20d00b4be308f5bef347
Ben Hudson <ben@hudsonsathome.com>
2025-10-24 10:52:53 -0400

    Add sparse incidence matrix to purc.utils

--- src/route_choice/purc/utils.py
+++ src/route_choice/purc/utils.py
@@ -19,6 +19,31 @@ def dense_incidence_matrix(
     return incidence
 
 
+def sparse_incidence_matrix(
+    edge_index: torch.Tensor,
+    n_nodes: int = None,
+    n_edges: int = None,
+    dtype: torch.dtype = torch.float32,
+):
+    if n_nodes is None:
+        n_nodes = torch.unique(edge_index).size(0)
+    if n_edges:
+        n_edges = edge_index.size(1)
+
+    edge_number = torch.arange(n_edges)
+
+    tails = -torch.ones_like(edge_index[0])
+    heads = torch.ones_like(edge_index[1])
+
+    tail_coords = torch.stack((edge_index[0], edge_number))
+    head_coords = torch.stack((edge_index[1], edge_number))
+
+    indices = torch.cat((tail_coords, head_coords), dim=-1)
+    values = torch.cat((tails, heads), dim=-1).to(dtype)
+
+    return torch.sparse_coo_tensor(indices, values, size=(n_nodes, n_edges))
+
+
 def load_purc_toy_network():
     G = nx.MultiDiGraph()
     G.add_node("o", pos=(0, 0))
