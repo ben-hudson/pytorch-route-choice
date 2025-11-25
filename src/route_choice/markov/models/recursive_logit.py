372f9ec2bad3cd7e065ef2eeb9dbd9fd74973925
Ben Hudson <ben@hudsonsathome.com>
2025-11-25 11:49:21 -0500

    scale rewards to ensure convergence

--- src/route_choice/markov/models/recursive_logit.py
+++ src/route_choice/markov/models/recursive_logit.py
@@ -1,5 +1,7 @@
 import torch
 
+from torch_geometric.utils import scatter
+
 from ..layers import EdgeProb, LinearFixedPoint
 
 
@@ -26,25 +28,43 @@ class RecursiveLogitRouteChoice(torch.nn.Module):
     def forward(
         self, edge_index: torch.Tensor, edge_feats: torch.Tensor, sink_node_mask: torch.Tensor, **solver_kwargs
     ):
-        # this isn't very efficient since we do .exp() after
-        rewards = -torch.nn.functional.softplus(self.encoder(edge_feats)).squeeze(-1)
+        unscaled_rewards = self.encoder(edge_feats).squeeze(-1)
 
-        values, edge_probs = self.get_values_and_probs(edge_index, rewards.exp(), sink_node_mask, **solver_kwargs)
-        return rewards, values, edge_probs
+        # we need to scale exp(reward) such that the sum over each row is < 1
+        # see: https://pubsonline.informs.org/doi/full/10.1287/trsc.2022.1145
+        exp_unscaled_rewards = unscaled_rewards.exp()
+        sum_over_rows = scatter(exp_unscaled_rewards, edge_index[0], dim=self.node_dim, reduce="sum")
+        exp_rewards = exp_unscaled_rewards / sum_over_rows.max()
+
+        values, edge_probs = self.get_values_and_probs(edge_index, exp_rewards, sink_node_mask, **solver_kwargs)
+        return exp_rewards.log(), values, edge_probs
 
     def get_values_and_probs(
         self, edge_index: torch.Tensor, exp_rewards: torch.Tensor, sink_node_mask: torch.Tensor, **solver_kwargs
     ):
+
         exp_values, _ = self.node_value(
-            edge_index, exp_rewards, sink_node_mask, sink_node_mask.clone(), **solver_kwargs
+            edge_index,
+            exp_rewards.unsqueeze(0),
+            sink_node_mask.unsqueeze(0),
+            sink_node_mask.clone().unsqueeze(0),
+            **solver_kwargs
         )
+        exp_values = exp_values.squeeze(0)
 
         edge_probs = self.edge_prob(edge_index, exp_rewards, exp_values, sink_node_mask)
 
         return exp_values.log(), edge_probs
 
     def get_flows(self, edge_index: torch.Tensor, edge_probs: torch.Tensor, demand: torch.Tensor, **solver_kwargs):
-        node_flows, _ = self.fixed_point(edge_index.flip(0), edge_probs, demand, demand.clone(), **solver_kwargs)
+        node_flows, _ = self.fixed_point(
+            edge_index.flip(0),
+            edge_probs.unsqueeze(0),
+            demand.unsqueeze(0),
+            demand.clone().unsqueeze(0),
+            **solver_kwargs
+        )
+        node_flows = node_flows.squeeze(0)
 
         edge_flows = node_flows.index_select(self.node_dim, edge_index[0]) * edge_probs
         return node_flows, edge_flows
